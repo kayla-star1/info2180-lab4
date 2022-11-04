625008c5182e530ac9f9e15cd8101c007a41d7cf
Kayla Swift <kayla.swift1302@gmail.com>
2022-11-04 17:44:44 -0500

    Update JS and HTML files

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 window.onload = function(){
-   let buttion = document.getElementsByClassName("btn")
-
-   buttion.addEventListener("click", handleSearch)
-
-   const httpRequest = new XMLHttpRequest();
-   let url = "http://localhost:8080/superheroes.php";
-   httpRequest.onreadystatechange = handleSearch;
-   httpRequest.open('GET', url);
-   httpRequest.send();
-
-   function handleSearch() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        let response = httpRequest.responseText;
-        alert(response);
-      } 
-      else {
-        alert('Error with the request.');
-      } 
+  
+   document.getElementById("btn").onclick= function(){handleButton()};
+   function handleButton() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("btn").innerHTML =
+        alert(this.responseText);
       }
-   }
+    };
+    xhttp.open("GET", "superheroes.php", true);
+    xhttp.send();
 
+  }
 }
