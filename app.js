e293e68634c4bd3c9ff3e6baa6c84e4d0c21f44d
Kayla Swift <kayla.swift1302@gmail.com>
2022-11-05 19:20:42 -0500

    Update JS and HTML files

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 window.onload = function(){
   
-   document.getElementById("btn").onclick= function(){handleButton()};
-   function handleButton() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("btn").innerHTML =
-        alert(this.responseText);
-      }
-    };
-    xhttp.open("GET", "superheroes.php", true);
-    xhttp.send();
+  document.getElementById("btn").onclick= function(){handleButton()};
+  function handleButton() {
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) {
+       alert(this.responseText);
+     }
+   };
+   xhttp.open("GET", "superheroes.php");
+   xhttp.send();
 
-  }
+ }
 }
